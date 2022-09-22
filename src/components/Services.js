044a0ce6e49b66a84efc636e6be25191e9522992
Jesan <foysal.jesan@gmail.com>
2022-09-22 21:28:14 +0600

    updated service card 2

--- src/components/Services.js
+++ src/components/Services.js
@@ -11,7 +11,7 @@ function Services() {
       <div className="body">
 
         <div className="card">
-          <div className="card-img"></div>
+          <div className="card-img1"></div>
           <div className="card-txt">
             <h4>Person 2 Person Delivery(P2P)</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero id velit laboriosam esse commodi? Iure, optio fugit sit distinctio sed minima facere nobis.</p>
@@ -27,7 +27,7 @@ function Services() {
 
         </div>
         <div className="card">
-          <div className="card-img"></div>
+          <div className="card-img2"></div>
           <div className="card-txt">
             <h4>Ware-House</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero id velit laboriosam esse commodi? Iure, optio fugit sit distinctio sed minima facere nobis.</p>
@@ -43,9 +43,9 @@ function Services() {
 
         </div>
         <div className="card">
-          <div className="card-img"></div>
+          <div className="card-img3"></div>
           <div className="card-txt">
-            <h4>Marchent Delivery Service</h4>
+            <h4>Merchant Delivery Service</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero id velit laboriosam esse commodi? Iure, optio fugit sit distinctio sed minima facere nobis.</p>
           </div>
           <div className="card-stats">
@@ -59,7 +59,7 @@ function Services() {
 
         </div>
         <div className="card">
-          <div className="card-img"></div>
+          <div className="card-img4"></div>
           <div className="card-txt">
             <h4>Percel Pick & Drop Service</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero id velit laboriosam esse commodi? Iure, optio fugit sit distinctio sed minima facere nobis.</p>
@@ -75,9 +75,9 @@ function Services() {
 
         </div>
         <div className="card">
-          <div className="card-img"></div>
+          <div className="card-img5"></div>
           <div className="card-txt">
-            <h4>Payment On Delivery(POD)</h4>
+            <h4>Cash On Delivery(COD)</h4>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero id velit laboriosam esse commodi? Iure, optio fugit sit distinctio sed minima facere nobis.</p>
           </div>
           <div className="card-stats">
